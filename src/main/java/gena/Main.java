0bb3cbfe320c2d9ef5fe2b5fa949ae90ce3f3cf3
alezalez8 <5446943@ukr.net>
2021-12-27 21:47:13 +0200

    add gena

--- src/main/java/gena/Main.java
+++ src/main/java/gena/Main.java
@@ -2,7 +2,7 @@ package gena;
 
 public class Main {
     public static void main(String[] args) {
-        String myName = "Hello Vlad" + "my name is Gena";
+        String myName = new String("Hello Vlad" + "my name is Gena");
         System.out.println(myName);
     }
 }
