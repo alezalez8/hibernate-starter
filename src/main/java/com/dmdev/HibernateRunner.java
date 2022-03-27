2ff7c6e12bf66d97aee46f60281e1363c0c71efc
alezalez8 <5446943@ukr.net>
2022-03-27 21:00:13 +0300

    lesson logging 2 07:20

--- src/main/java/com/dmdev/HibernateRunner.java
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -4,16 +4,22 @@ import com.dmdev.entity.User;
 import com.dmdev.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.SQLException;
 
 public class HibernateRunner {
+    private static Logger log = LoggerFactory.getLogger(HibernateRunner.class);
+
     public static void main(String[] args) throws SQLException {
         User user = User.builder()
                 .username("gena1@gmail.com")
                 .lastname("Ivanov")
                 .firstname("Ivan")
                 .build();
+
+        log.info("User entity is in transient state, object: {}", user);
 
         try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
             try (Session session1 = sessionFactory.openSession()) {
